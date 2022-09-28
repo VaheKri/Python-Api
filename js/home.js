e34918fa3f738335bca0e4cc19bb54707ae94fa4
VaheKri <vahe.krikorian@edu.esiee-it.fr>
2022-09-28 20:55:29 +0200

    formulaire css animation and js and
    add button for close the form page and y have create css and javascript animation

--- js/home.js
+++ js/home.js
@@ -5,31 +5,80 @@ var espagne_path = document.getElementById("espagne-path");
 var portugal = document.getElementById("portugal");
 var portugal_path = document.getElementById("portugal-path");
 var pays_path = document.getElementsByClassName("pays-path");
-var form = document.getElementsByClassName("form");
+var form = document.querySelector(".form");
+var close = document.querySelector("#close");
+var nom_pays = document.getElementById("Nom-du-Pays");
 //le button envoyé du formulaire
 var submit = document.getElementById("btnForm");
 //var slogan
 var slogan = document.getElementById("slogan");
-
-//cacher le formulaire si on click sur le bouton envoyer
-if (submit.clicked == true) {
-  form[0].style.display = "none";
-}
+var population = document.getElementById("Population");
 
 //si on click sur france-path alors on afficher le formulaire
 france_path.addEventListener("click", function () {
   //display form
-  form[0].style.display = "block";
+  form.style.opacity = "1";
+  form.style.zIndex = "10";
+  form.style.userSelect = "all";
+  close.style.opacity = "1";
+  close.style.zIndex = "10";
+  close.style.userSelect = "contain";
+  nom_pays.value = "France";
+  population.focus();
+});
+
+close.addEventListener("click", function () {
+  form.style.opacity = "0";
+  form.style.zIndex = "-1";
+  form.style.userSelect = "none";
 });
 
 //si on click sur espagne-path alors on afficher le formulaire
 espagne_path.addEventListener("click", function () {
   //display form
-  form[0].style.display = "block";
+  form.style.opacity = "1";
+  form.style.zIndex = "10";
+  form.style.userSelect = "all";
+  close.style.opacity = "1";
+  close.style.zIndex = "10";
+  close.style.userSelect = "contain";
+  nom_pays.value = "Espagne";
+  population.focus();
 });
 
 //si on click sur portugal-path alors on afficher le formulaire
 portugal_path.addEventListener("click", function () {
   //display form
-  form[0].style.display = "block";
+  form.style.opacity = "1";
+  form.style.zIndex = "10";
+  form.style.userSelect = "all";
+  close.style.opacity = "1";
+  close.style.zIndex = "10";
+  close.style.userSelect = "contain";
+  nom_pays.value = "Portugal";
+  population.focus();
+});
+
+france_path.addEventListener('mouseover', function(e) {
+    france.style.zIndex = 5;
+});
+
+france_path.addEventListener('mouseleave', function(e) {
+    france.style.zIndex = 1;
+});
+
+espagne_path.addEventListener('mouseover', function(e) {
+    espagne.style.zIndex = 5;
+});
+
+espagne_path.addEventListener('mouseleave', function(e) {
+    espagne.style.zIndex = 1;
+});
+
+portugal_path.addEventListener('mouseover', function(e) {
+    portugal.style.zIndex = 5;
+});
+
+portugal_path.addEventListener('mouseleave', function(e) {
+    portugal.style.zIndex = 1;
 });
