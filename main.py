5bcbe82961d5ac8c7d9160a4da3f3002797cb90f
Maxime Nicolas <maximenicolas1512@gmail.com>
2022-09-28 15:31:03 +0200

    get population id

--- main.py
+++ main.py
@@ -1,16 +1,94 @@
-# This is a sample Python script.
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from database import get_db, set_db
+from country import Country
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+app = FastAPI()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# /monuments/{id}
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# /populations/{id}
+# /monuments/{id}
+
+# Étienne
+@app.post('/countrys', status_code=201)
+def create_country(country: Country):
+    data = get_db()
+
+    country = {
+        "id": country.id,
+        "name": country.name,
+        "capital": country.capital,
+        "president": country.president,
+        "national_day": country.national_day,
+        "currency": country.currency,
+        "flag": country.flag,
+        "area": country.area
+    }
+
+    data['countrys'].append(country)
+    set_db(data)
+    return
+
+
+# Étienne
+@app.get('/countrys', status_code=200)
+def get_countrys():
+    countrys = get_db()['countrys']
+    return JSONResponse(content=jsonable_encoder(countrys))
+
+
+# Maxime & Étienne
+@app.get('/countrys/{name}', status_code=200)
+def get_country(name: str | None = None):
+    countrys = get_db()['countrys']
+    data = {}
+
+    if name is None:
+        return data
+
+    name = name.lower()
+
+    for index, country in enumerate(countrys):
+        if name == country['name'].lower():
+            data = countrys[index]
+
+    return JSONResponse(content=jsonable_encoder(data))
+
+
+# Étienne
+@app.delete('/countrys/{name}', status_code=204)
+def delete_country(name: str | None = None):
+    data = get_db()
+
+    if name is None:
+        return
+
+    name = name.lower()
+
+    for index, country in enumerate(data['countrys']):
+        if name == country['name'].lower():
+            del data['countrys'][index]
+            set_db(data)
+    return
+
+
+#maxime
+@app.get('/populations/{id}', status_code=200)
+def get_population(id: int | None = None):
+    populations = get_db()['populations']
+    data = {}
+
+    if id is None:
+        return data
+
+    for index, population in enumerate(populations):
+        if id == population['id']:
+            data = population
+
+    return JSONResponse(content=jsonable_encoder(data))
+
+
